3480443428f5743381faa9bf67fb0e7999578a46
Justin Karimi <jekhokie@gmail.com>
2012-07-25 11:54:15 -0400

    Add Handling for Revisionables that are New
    Add try statements for attempting to access a revisionable's associated
    record since new revisionable objects may not yet have records.

--- lib/revisionable/plugins/revisionable.rb
+++ lib/revisionable/plugins/revisionable.rb
@@ -25,27 +25,27 @@ module Revisionable
     belongs_to :version, :class_name => "revision"
     many :revisions, :as => :revisionable do
       def tagged(tag)
-        first(:tag => tag).record
+        first(:tag => tag).try :record
       end
 
       def current
-        sort(:saved_at).last.record
+        sort(:saved_at).last.try :record
       end
 
       def at(point)
         if point.is_a? Date
-          first(:saved_at => point).record
+          first(:saved_at => point).try :record
         elsif point.is_a? Symbol
           case point
           when :current
-            sort(:saved_at).last.record
+            sort(:saved_at).last.try :record
           when :first
-            sort(:saved_at).first.record
+            sort(:saved_at).first.try :record
           when :last
-            sort(:saved_at).all[sort(:saved_at).all.index(proxy_owner.version) - 1].try(:record)
+            sort(:saved_at).all[sort(:saved_at).all.index(proxy_owner.version) - 1].try :record
           end
         elsif point.is_a? Integer
-          sort(:saved_at)[point].record
+          sort(:saved_at)[point].try :record
         end
       end
     end
